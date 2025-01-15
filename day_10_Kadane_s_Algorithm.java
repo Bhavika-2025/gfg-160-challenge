fc27aef5ffca0bb38a52caa17173946007e91824
Bhavika Prakash Chavan <bhavikachavan852004@gmail.com>
2025-01-15 06:15:05 +0530

    Create day_10_Kadane_s_Algorithm.java

---
+++ day_10_Kadane_s_Algorithm.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long maxSubarraySum(int[] arr) {
+        long maxh = 0, maxf = Long.MIN_VALUE;
+
+        for (int num : arr) {
+            maxh = Math.max(num, maxh + num);
+            maxf = Math.max(maxf, maxh);
+        }
+
+        return maxf;
+    }
+}
+
